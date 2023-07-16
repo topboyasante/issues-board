053dc44c7f96adca503a9e23d8d05a97d2cbb8d2
Nana K <73991180+topboyasante@users.noreply.github.com>
2023-07-16 13:56:39 +0000

    Added Create List Item functions

--- src/pages/tables/[name].tsx
+++ src/pages/tables/[name].tsx
@@ -8,7 +8,10 @@ import { List, Table } from "@/types";
 import toast from "react-hot-toast";
 
 function Index() {
-  let [isOpen, setIsOpen] = useState<boolean>(false);
+  let [isAddListOpen, setAddListIsOpen] = useState<boolean>(false);
+  let [isAddListItemOpen, setAddListItemIsOpen] = useState<boolean>(false);
+  let [selectedTableList, setSelectedTableList] = useState<List>();
+
   const controls = useDragControls();
 
   const tables = useTableStore((state) => state.tables);
@@ -18,41 +21,77 @@ function Index() {
   const currentTable = tables.find((item) => item.name === name) as Table;
   const addNewList = useTableStore((state) => state.createList);
 
+  const addNewListItem = useTableStore((state) => state.createListItem);
+
   const {
-    register,
-    handleSubmit,
-    formState: { errors },
+    register: registerList,
+    handleSubmit: handleSubmitList,
+    formState: { errors: listErrors },
   } = useForm<List>();
-  const onSubmit: SubmitHandler<List> = (data) => {
-    data.items=[]
-    addNewList(currentTable,data)
-    setIsOpen(false)
+  const onSubmitList: SubmitHandler<List> = (data) => {
+    data.items = [];
+    addNewList(currentTable, data);
+    setAddListIsOpen(false);
     toast.success("Success!");
+  };
+
+  type addListItemInput = {
+    name: string;
+  };
+
+  const {
+    register: registerListItem,
+    handleSubmit: handleSubmitListItem,
+    formState: { errors: listItemErrors },
+  } = useForm<addListItemInput>();
+  const onListItemSubmit: SubmitHandler<addListItemInput> = (data) => {
+    console.log(data);
+    setAddListItemIsOpen(false);
+    const selectedList = currentTable.lists.find(
+      (list) => list.name === selectedTableList?.name
+    );
+    if (selectedList) {
+      addNewListItem(currentTable, selectedList, data.name);
+      toast.success("Success!");
+    }
   };
 
   return (
     <>
       <main className="pl-[15vw]">
-        <section className="">
+        <section>
           <section className="h-[5vh] px-5 py-8 flex justify-between items-center">
             <h1 className="font-bold text-2xl">{name}</h1>
             <button
               className="bg-black text-white px-4 py-2"
               onClick={() => {
-                setIsOpen(true);
+                setAddListIsOpen(true);
               }}
             >
               <p>Add List</p>
             </button>
           </section>
           <section className="relative overflow-scroll w-full h-[88vh] my-3 p-5 scrollbar-hide">
-            <section className="absolute">
-              {currentTable?.lists.length ?? 0 ? (
-                currentTable?.lists.map((item, index) => (
-                  <motion.div key={index} drag dragControls={controls} className="my-3">
-                    <h1 className="border w-[300px] text-center p-3 bg-white">
-                      {item.name}
-                    </h1>
+            <section className="absolute flex gap-3">
+              {currentTable?.lists.length ? (
+                currentTable.lists.map((item, index) => (
+                  <motion.div
+                    key={index}
+                    drag
+                    dragControls={controls}
+                    className="my-3"
+                  >
+                    <div className="flex w-[300px] border p-3 justify-between items-center">
+                      <h1 className="">{item.name}</h1>
+                      <button
+                        onClick={() => {
+                          setAddListItemIsOpen(true);
+                          setSelectedTableList(item);
+                        }}
+                      >
+                        <p className="text-xl">+</p>
+                      </button>
+                    </div>
                     <section className="p-3 bg-gray-100">
                       {item.items.map((item, index) => (
                         <motion.div key={index} drag dragControls={controls}>
@@ -72,9 +111,10 @@ function Index() {
         </section>
       </main>
 
+      {/* Add New List Modal */}
       <Dialog
-        open={isOpen}
-        onClose={() => setIsOpen(false)}
+        open={isAddListOpen}
+        onClose={() => setAddListIsOpen(false)}
         className="relative z-50"
       >
         <div className="fixed inset-0 bg-black/30" aria-hidden="true" />
@@ -85,15 +125,17 @@ function Index() {
               <h1 className="font-bold">Add a List</h1>
             </Dialog.Title>
             <section>
-              <form onSubmit={handleSubmit(onSubmit)}>
-                <label htmlFor="table_name text-xl">Name:</label>
+              <form onSubmit={handleSubmitList(onSubmitList)}>
+                <label htmlFor="table_name" className="text-xl">
+                  Name:
+                </label>
                 <br />
                 <input
                   type="text"
                   className="border border-black rounded px-4 py-2 my-5"
-                  {...register("name", { required: true })}
+                  {...registerList("name", { required: true })}
                 />
-                {errors.name && (
+                {listErrors.name && (
                   <p className="text-red-500 text-sm">This field is required</p>
                 )}
                 <br />
@@ -108,12 +150,48 @@ function Index() {
           </Dialog.Panel>
         </div>
       </Dialog>
+
+      {/* Add new List Item Modal */}
+      <Dialog
+        open={isAddListItemOpen}
+        onClose={() => setAddListItemIsOpen(false)}
+        className="relative z-50"
+      >
+        <div className="fixed inset-0 bg-black/30" aria-hidden="true" />
+
+        <div className="fixed inset-0 flex items-center justify-center p-4">
+          <Dialog.Panel className="mx-auto max-w-sm rounded bg-white p-5">
+            <Dialog.Title>
+              <h1 className="font-bold">Add a List Item</h1>
+            </Dialog.Title>
+            <section>
+              <form onSubmit={handleSubmitListItem(onListItemSubmit)}>
+                <label htmlFor="item_name" className="text-xl">
+                  Name:
+                </label>
+                <br />
+                <input
+                  type="text"
+                  className="border border-black rounded px-4 py-2 my-5"
+                  {...registerListItem("name", { required: false })}
+                />
+                {listItemErrors.name && (
+                  <p className="text-red-500 text-sm">This field is required</p>
+                )}
+                <br />
+                <button
+                  className="bg-black text-white px-4 py-2 rounded my-5"
+                  type="submit"
+                >
+                  <p>Create List Item</p>
+                </button>
+              </form>
+            </section>
+          </Dialog.Panel>
+        </div>
+      </Dialog>
     </>
   );
 }
 
 export default Index;
-function register(arg0: string, arg1: { required: boolean; }): import("react").JSX.IntrinsicAttributes & import("react").ClassAttributes<HTMLInputElement> & import("react").InputHTMLAttributes<HTMLInputElement> {
-  throw new Error("Function not implemented.");
-}
-
--- src/zustand/Store.ts
+++ src/zustand/Store.ts
@@ -6,6 +6,7 @@ type Store = {
   tables: Table[];
   createTable: (table: Table) => void;
   createList: (table: Table, List: List) => void;
+  createListItem: (table: Table, List: List, item:string) => void;
 };
 
 const useTableStore = create<Store>()(
@@ -44,6 +45,41 @@ const useTableStore = create<Store>()(
           }
         });
       },
+
+      createListItem: (table, list, item) => {
+        set((state) => {
+          //Mapping through all the tables
+          const updatedTables = state.tables.map((tItem) => {
+            //Search for the current table we want to update
+            if (tItem.name === table.name) {
+              //Map through all that table's lists
+              const updatedLists = tItem.lists.map((lItem) => {
+                //Search for the list we want to update
+                if (lItem.name === list.name) {
+                  //Push the list item we want to add to the list items array
+                  return {
+                    ...lItem,
+                    items: [...lItem.items, item],
+                  };
+                }
+                // return each member of the updatedLists Array
+                return lItem;
+              });
+              //return each item of the updatedTables Array, update the list of each table with the new list
+              return {
+                ...tItem,
+                lists: updatedLists,
+              };
+            }
+            return tItem;
+          });
+          //update all the tables in the state
+          return {
+            tables: updatedTables,
+          };
+        });
+      },
+
     }), //end of setter function for create
     {
       name: "tables",
